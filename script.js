abcf7db5af90e16783da776a1b2ab95866dbdee7
Marie <mcari1311@gmail.com>
2022-07-13 11:25:25 -0400

    initilized elements

---
+++ script.js
@@ -0,0 +1,6 @@
+let player = document.getElementById('player')
+let computer = document.getElementById('computer')
+let results = document.querySelector('.results')
+let game = document.querySelector('.game')
+
+let count = 0
