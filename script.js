7b807fc7cbb0e45ab9adc9c97474bd7018cb121d
Marie <mcari1311@gmail.com>
2022-07-13 13:16:37 -0400

    basic game finished

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let player = document.getElementById('player')
 let computer = document.getElementById('computer')
 let results = document.querySelector('.results')
 let game = document.querySelector('.game')
+let score = document.getElementById('score')
+let nextLevelButton = document.getElementById('nextLevel')
 
 let count = 0
 
@@ -10,6 +12,8 @@ computer.addEventListener("animationiteration", function() {
     let randomDirection = ((Math.floor(Math.random() * 3))*130)
     computer.style.left = randomDirection+"px"
     count++
+    score.innerHTML = `SCORE: ${count}`
+
 })
 
 //make player car able to move
@@ -27,3 +31,16 @@ window.addEventListener("keydown", function(e) {
             }
     }
 })
+
+//what happens if collison -> game over 
+setInterval(function Gameover() {
+    let computerTop = parseInt(window.getComputedStyle(computer).getPropertyValue('top'))
+    let computerLeft = parseInt(window.getComputedStyle(computer).getPropertyValue('left')) 
+    let playerleft = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
+
+    if((computerLeft === playerleft)&&(computerTop>250)&&(computerTop<450)) {
+        game.style.display = 'none'
+        nextLevelButton.style.display = 'block'
+        score.innerHTML = `SCORE: ${count}`
+    }
+}, 5)
