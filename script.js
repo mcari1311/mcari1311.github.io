9352b33f1fda496936d89af1174c062db30b6735
Marie <mcari1311@gmail.com>
2022-07-15 14:06:55 -0400

    styled homepage

---
+++ script.js
@@ -0,0 +1,3 @@
+function explain() {
+    alert ('Using the left and right buttons on your keyboard, try to avoid the moving car in time! You get 1 point added to your score each time you doged an incoming car!')
+}
