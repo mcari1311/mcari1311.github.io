a34e6034b138e68e744ea8cd8bdf1b3f6b3349f3
Marie <mcari1311@gmail.com>
2022-07-14 20:42:55 -0400

    added second computer player

--- level3.js
+++ level3.js
@@ -5,6 +5,7 @@ let game = document.querySelector('.game')
 let score = document.getElementById('score')
 let nextLevelButton = document.getElementById('nextLevel')
 let characterBox = document.querySelector('.chooseCharacter')
+let computer2 = document.getElementById('computer2')
 let count = 0
 
 function changeCar() {
@@ -21,7 +22,15 @@ function changeCarB() {
 
 // randomly move computer car to each column and keep score of how many pass
 computer.addEventListener("animationiteration", function() {
-    let randomDirection = ((Math.floor(Math.random() * 3))*130)
+    let randomDirection = ((Math.floor(Math.random() * 4))*130)
+    computer.style.left = randomDirection+"px"
+    count++
+    score.innerHTML = `SCORE: ${count}`
+
+})
+// again for computer 2
+computer2.addEventListener("animationiteration", function() {
+    let randomDirection = ((Math.floor(Math.random() * 4))*130)
     computer.style.left = randomDirection+"px"
     count++
     score.innerHTML = `SCORE: ${count}`
@@ -57,5 +66,19 @@ setInterval(function Gameover() {
         characterBox.style.display='none'
     }
 }, 5)
+
+setInterval(function Gameover() {
+    let computer2Top = parseInt(window.getComputedStyle(computer2).getPropertyValue('top'))
+    let computer2Left = parseInt(window.getComputedStyle(computer2).getPropertyValue('left')) 
+    let playerleft = parseInt(window.getComputedStyle(player).getPropertyValue('left'))
+
+    if((computer2Left === playerleft)&&(computer2Top>250)&&(computer2Top<450)) {
+        game.style.display = 'none'
+        nextLevelButton.style.display = 'block'
+        score.innerHTML = `SCORE: ${count}`
+        characterBox.style.display='none'
+    }
+}, 5)
+
 
 
